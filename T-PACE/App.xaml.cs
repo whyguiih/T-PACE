85449da9adaca98ea32693b42115b43d352830e2
whyguiih <whyguiih@gmail.com>
2026-08-10 16:49:05 -0300

    Adicionar arquivos de projeto.

---
+++ T-PACE/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace T_PACE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
